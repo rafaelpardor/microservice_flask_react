83f927eb1faaca8c722fc5a5c601b4d9177642e1
Rafael Pardo <rafaelpardor@protonmail.com>
2021-06-13 00:05:39 -0500

    Init Flask Project

---
+++ testdriven-app/services/users/project/config.py
@@ -0,0 +1,19 @@
+class BaseConfig:
+    """Base Configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig:
+    """Development Configuration"""
+    pass
+
+
+class TestingConfig:
+    """Testing Configuration"""
+    TESTING = True
+
+
+class ProductionConfig:
+    """Production Configuration"""
+    pass
+
