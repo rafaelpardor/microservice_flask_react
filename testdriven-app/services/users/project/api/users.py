8af50b85e68e6ae09dca10f72f58790d7a028956
Rafael Pardo <rafaelpardor@protonmail.com>
2021-06-15 01:18:09 -0500

    Adding more tests for users endpoint

--- testdriven-app/services/users/project/api/users.py
+++ testdriven-app/services/users/project/api/users.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy import exc
 
 from project.api.models import User
 from project import db
@@ -15,13 +16,33 @@ def ping_pong():
 @users_blueprint.route('/users', methods=['POST'])
 def add_user():
     post_data = request.get_json()
+    response_object = {
+        'status': '',
+        'message': ''
+    }
+    if not post_data or post_data.get('username') == None:
+        response_object['status'] = 'fail'
+        response_object['message'] = 'Invalid payload.'
+        return jsonify(response_object), 400
+
     username = post_data.get('username')
     email = post_data.get('email')
-    db.session.add(User(username=username, email=email))
-    db.session.commit()
-    response_object = {
-        'status': 'success',
-        'message': f'{email} was added!'
-    }
-    return jsonify(response_object), 201
+    try:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            response_object['status'] = 'fail'
+            response_object['message'] = 'Email already exist.'
+            return jsonify(response_object), 400
+
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+        response_object['status'] = 'success',
+        response_object['message'] = f'{email} was added!'
+        return jsonify(response_object), 201
+
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object['status'] = 'fail'
+        response_object['message'] = f'{e}'
+        return jsonify(response_object), 400
 
