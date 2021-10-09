360adab7f6bc1df8c7d80c9a6c044c77c7f4d536
Rafael Pardo <rafaelpardor@protonmail.com>
2021-10-09 17:50:55 -0500

    Adding index template, with submit form Adding tests:   - test_index_no_users   - test_index_with_users   - test_index_submit_user

--- testdriven-app/services/users/project/api/users.py
+++ testdriven-app/services/users/project/api/users.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from sqlalchemy import exc
 
 from project.api.models import User
 from project import db
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
 @users_blueprint.route('/users/ping', methods=['GET'])
 def ping_pong():
@@ -87,4 +87,16 @@ def get_sinlge_user(user_id):
         response_object['status'] = 'fail'
         response_object['message'] = 'User does not exist.'
         return jsonify(response_object), 404
+
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
--- testdriven-app/services/users/project/tests/test_users.py
+++ testdriven-app/services/users/project/tests/test_users.py
@@ -132,6 +132,34 @@ class TestUserService(BaseTestCase):
             self.assertIn('johnwick@gmail.com', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
+    def test_index_no_users(self):
+        """Ensure the index route behaves correctly when no users have been added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>All Users</h1>', response.data)
+        self.assertIn(b'<p>No users</p>', response.data)
+
+    def test_index_with_users(self):
+        """Ensure the index route behaves correctly when users have been added to the database."""
+        add_user('johndoe' , 'johndoe@gmail.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users</p>', response.data)
+            self.assertIn(b'johndoe', response.data)
+
+    def test_index_submit_user(self):
+        with self.client:
+            response = self.client.post(
+                '/',
+                data=dict(username='johndoe', email='johndoe@email.com'),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users</p>', response.data)
+            self.assertIn(b'johndoe', response.data)
 
 if __name__ == '__main__':
     unittest.main()
