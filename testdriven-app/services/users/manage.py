fb0e8005c010ae2af7f7bc7e9b96652ce7bd2464
Rafael Pardo <rafaelpardor@protonmail.com>
2021-10-09 17:53:11 -0500

    Adding test coverage and linter

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest import result
 
 from flask.cli import FlaskGroup
+import coverage
 
 from project import create_app, db
 from project.api.models import User
@@ -9,6 +11,15 @@ from project.api.models import User
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py'
+    ]
+)
+COV.start()
 
 @cli.command("recreate_db")
 def recreate_db():
@@ -23,6 +34,21 @@ def test():
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase
         return 0
     return 1
 
