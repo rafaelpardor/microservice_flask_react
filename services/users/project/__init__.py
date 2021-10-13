2ae0a3fd79024694c8f71015f6d9e3ca6a5c0234
Rafael Pardo <rafaelpardor@protonmail.com>
2021-10-12 20:38:56 -0500

    Adding CORS support

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
@@ -12,6 +13,7 @@ def create_app(script_info=None):
     app = Flask(__name__)
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
+    CORS(app)
 
     db.init_app(app)
     toolbar.init_app(app)
